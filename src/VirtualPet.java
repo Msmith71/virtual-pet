fd04584b6c8888c1c1c342574b3d1ea6baca3f1a
Michael Smith <mjsmitths@gmail.com>
2017-09-07 20:45:48 -0400

    pet

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,4 +1,60 @@
 
 public class VirtualPet {
+	String petName;
+	double hunger;
+	double thirst;
+	double happiness;
+	double cleanliness;
+	
+	double eat = 20.0;
+	double drink = 20.0;
+	double play = 30.0;
+	double clean = 50.0;
+	
+	boolean alive = true;
+	
+	//hunger decreases by 5
+	void updatetHunger() {
+		hunger -= 5;
+	}
+	//thirst decreases by 6.5
+	void updateThirst() {
+		thirst -= 6.5;
+	}
+	
+	//Happiness decreases by 6
+	void updatetHappiness() {
+		happiness -= 6;
+	}
+	
+	//Cleanliness decreases by 20
+	void updateCleanliness() {
+		cleanliness -= 20;
+	}
+	
+//////////////////////////////////////////////////////////////////////////////////////////
 
+	
+	//pet eats
+	void hungerEat() {
+		hunger += eat;
+	}
+	//pet drinks
+	void thirstDrink() {
+		thirst += drink;
+	}
+	
+	//pet plays
+	void happyPlay() {
+		happiness += play;
+	}
+	
+	//pet is cleaned
+	void cleanlinessClean() {
+		cleanliness += clean;
+	}
+	
+
+	
+	
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -6,21 +6,92 @@ public class VirtualPetApp {
 		
 		Scanner input = new Scanner(System.in);
 		
-		int userInput = input.nextInt();
-		switch (userInput) {
+		VirtualPet belly = new VirtualPet();
+		VirtualPet water = new VirtualPet();
+		VirtualPet mood = new VirtualPet();
+		VirtualPet hygiene = new VirtualPet();
 		
-		case 1:
-			System.out.println("Check Status");
-		case 2:
-			System.out.println("Feed");
-		case 3:
-			System.out.println("Drink");
-		case 4:
-			System.out.println("Clean");
-		case 5:
-			System.out.println("Play with");
+		belly.hunger = 100.00;
+		water.thirst = 100.00;
+		mood.happiness = 100.00;
+		hygiene.cleanliness = 100.00;
+		
+
+				
+
+
+		
+		//pet name
+		System.out.println("Please choose a name for your pet.");
+		
+		String petName = input.nextLine();
+		
+		boolean alive = true;
+		
+		
+		while (alive) {
+			System.out.println("========================");
+			System.out.println("What would you like to do with " + petName);
+			System.out.println("1. Feed");
+			System.out.println("2. Give water");
+			System.out.println("3. Play"); 
+			System.out.println("4. Bathe"); 
+			System.out.println("5. Nothing"); 
+			System.out.println("========================");
+
+			int choice = input.nextInt(); 
+
+
+			switch (choice) {
+
+	
+				//feed
+				case 1:
+					if (choice == 1) {
+						belly.hungerEat();
+						
+						System.out.println("You give " + petName + " a tastey treat!");
+						System.out.println(petName + "'s current hunger is now " + belly.hunger);
+					}
+					break;
+					
+				//drink
+				case 2:
+					if (choice == 2) {
+						water.thirstDrink();
+						
+						System.out.println("You give " + petName + " a bowl of water!");
+						System.out.println(petName + "'s current thirst is now " + water.thirst);
+					}
+					break;
+					
+				//play
+				case 3:
+					if (choice == 3) {
+						mood.happyPlay();
+						
+						System.out.println("You play with " + petName + "!");
+						System.out.println(petName + "'s current happiness is now " + mood.happiness);
+					}
+					break;
+
+				case 4:
+					if (choice == 4) {
+						hygiene.cleanlinessClean();
+						
+						System.out.println("You give " + petName + " a hot bathe!");
+						System.out.println(petName + "'s cleanliness is " + hygiene.cleanliness);
+					}
+					break;
+
+				
+
+				
+
+			}				 	
+			
+
 		}
-		
 	}
 
 }
